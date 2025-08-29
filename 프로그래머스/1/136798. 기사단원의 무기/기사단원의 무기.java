5e7edd88a84ffc7a19cb589f12607faf1591090e
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2025-08-29 19:09:41 +0900

    [level 1] Title: 기사단원의 무기, Time: 8.37 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,18 +1,6 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int[] cnt = new int[number + 1];
-
-        for (int d = 1; d <= number; d++) {
-            for (int m = d; m <= number; m += d) {
-                cnt[m]++;
-            }
-        }
-
         int answer = 0;
-        for (int i = 1; i <= number; i++) {
-            int atk = cnt[i] > limit ? power : cnt[i];
-            answer += atk;
-        }
         return answer;
     }
 }
