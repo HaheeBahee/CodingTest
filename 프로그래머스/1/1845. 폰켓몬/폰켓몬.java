750881b485f049d8e2d7ebb99a4947ed64a6cd3f
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2025-09-03 20:19:29 +0900

    [level 1] Title: 폰켓몬, Time: 6.04 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,13 +1,14 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
+
+        Set<Integer> kinds = new HashSet<>();
         for (int n : nums) {
-            // 종류별 빈도수 카운트 (없으면 0에서 시작해 +1)
-            freq.put(n, freq.getOrDefault(n, 0) + 1);
+            kinds.add(n);
         }
-        int kinds = freq.size();        // 서로 다른 종류 수
-        int canPick = nums.length / 2;  // 가져갈 수 있는 수
-        return Math.min(kinds, canPick);
+
+        int canPick = nums.length / 2;   
+        return Math.min(kinds.size(), canPick);
     }
 }
