5cd3414dd17138c024440b73f47853518854a0ac
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2025-10-02 23:11:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 38.99 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,11 +1,27 @@
+//연속적인 숫자는 하나만 남기고 제거
+//순서 유지 
+
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        int[] answer = {};
-        
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+    public int[] solution(int[] arr) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.add(arr[0]); 
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            //Rear 확인
+            if (arr[i] != deque.peekLast()) {
+                deque.add(arr[i]);
+            }
+        }
+
+        // Deque → int[]
+        int[] answer = new int[deque.size()];
+        int i = 0;
+        while (!deque.isEmpty()) {
+            answer[i++] = deque.pollFirst(); 
+        }
 
         return answer;
     }
