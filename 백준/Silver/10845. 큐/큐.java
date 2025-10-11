100ba54422134909af54c7d21513d3c284e4824f
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2025-10-11 18:59:24 +0900

    [Silver IV] Title: 큐, Time: 364 ms, Memory: 36248 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine(); 
+
+        Deque<Integer> q = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            String line = sc.nextLine();
+
+            if (line.startsWith("push")) {
+                int x = Integer.parseInt(line.substring(5));
+                q.addLast(x);
+            } else if (line.equals("pop")) {
+                sb.append(q.isEmpty() ? -1 : q.pollFirst()).append('\n');
+            } else if (line.equals("size")) {
+                sb.append(q.size()).append('\n');
+            } else if (line.equals("empty")) {
+                sb.append(q.isEmpty() ? 1 : 0).append('\n');
+            } else if (line.equals("front")) {
+                sb.append(q.isEmpty() ? -1 : q.peekFirst()).append('\n');
+            } else if (line.equals("back")) {
+                sb.append(q.isEmpty() ? -1 : q.peekLast()).append('\n');
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
+
