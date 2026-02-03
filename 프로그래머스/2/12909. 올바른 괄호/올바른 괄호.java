7e12c8b32c95746780c2fe1b196f3015709c9cb0
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2026-02-03 18:53:02 +0900

    [level 2] Title: 올바른 괄호, Time: 6.11 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,21 @@
-import java.util.Stack;
+//1. 즉시 실패 조건 : 반드시 ) 개수는 (보다 크면 안된다. 
+//2. 종료 조건 : (개수 = )개수
+//괄호 종류 1개 -> balance 
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        
-        Stack<Character> stack = new Stack<>();
-        
-        for(char a : s.toCharArray()){
-            if(a == '(') stack.push(a);
-            else{
-                if(stack.isEmpty()){
-                return false; 
-                }else{
-                    stack.pop();
-                }
-            } 
-                
-                
+        int balance = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                balance++;
+            } else { // ')'
+                if (--balance < 0) return false; // 닫을 '('가 없음
+            }
         }
 
-        return stack.isEmpty();
+        return balance == 0;
     }
 }
