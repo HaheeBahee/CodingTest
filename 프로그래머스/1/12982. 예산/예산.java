121ab95ce1016164023e426a237fab2a1c506f5d
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2026-04-05 21:20:47 +0900

    [level 1] Title: 예산, Time: 1.26 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,19 +1,22 @@
+// 전체 예산 고정. 최대한 많은 부서 지원. => 적은 예산 순으로 정렬 => 버짓 이하 만큼 반복
+
 import java.util.Arrays;
 
 class Solution {
     public int solution(int[] d, int budget) {
-        Arrays.sort(d); 
+        
+        Arrays.sort(d);
+        int total = 0; 
         int count = 0;
-        int total = 0;
-
-        for (int cost : d) {
-            if (total + cost <= budget) { 
-                total += cost;
+        
+        for(int i = 0; i < d.length ; i++){
+            total += d[i];
+            if(total <= budget){
                 count++;
-            } else { 
+            }else{
                 break;
             }
-        }
+        }  
         return count;
     }
 }
