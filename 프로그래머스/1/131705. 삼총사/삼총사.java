569af030ec3d76c17eefa6f45e2f4aad3463d75b
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2026-04-05 20:54:20 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,19 +1,23 @@
+// 세 정수 합했을 때 0 => 삼총사
+// 삼총사 만들 수 있는 여러가지 조합
+
+
 class Solution {
     public int solution(int[] number) {
-        int answer = 0;
-        int n = number.length;
-
-        // 서로 다른 세 학생 뽑기
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        answer++;
+        
+        int result = 0;
+        
+        for(int i = 0 ; i + 2 < number.length; i++){
+            for(int j = i + 1; j + 1 < number.length; j++){
+                for(int k = j + 1 ; k < number.length; k++){
+                    if(number[i] + number[j] + number[k] == 0){
+                        result++;
                     }
                 }
             }
         }
+    
 
-        return answer;
+        return result;
     }
 }
