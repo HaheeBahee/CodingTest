61af5c03ceadb8544c21f1833e4840ff55942043
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2026-04-22 22:55:04 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,31 +1,24 @@
+// 입력한 아이디와 패스워드 배열  - id_pw
+// 회원들의 정보가 담긴 2차원 배열 - db
+// 아이디와 비번 모두 일치하는 회원 있으면 login, 아이디 일치 없으면 fail, 아이디 일치하지만 비번 일치 없으면 wrong pw
+
 
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        String answer = "";
-        boolean idCorrect = false;
-        boolean pwCorrect = false; //비밀번호 맞는지 아닌지
         
-        for(int i = 0; i < db.length; i++){
-            //id가 같은지 확인
-            if(id_pw[0].equals(db[i][0])){
-                //id가 같으면, password가 같음
-                if(id_pw[1].equals(db[i][1])){
-                    answer ="login";
-                    pwCorrect = true; // 비밀번호도 맞음
-                    break; // 더 볼 필요 없음
-                }else {//password가 다름
-                    idCorrect = true;
-                    pwCorrect = false; 
+        String id = id_pw[0];
+        String pw = id_pw[1];
+        
+        for(int i = 0 ; i < db.length ; i++){
+            if (id.equals(db[i][0])){
+                if (pw.equals(db[i][1])){
+                    return "login";
+                }else{
+                    return "wrong pw";
                 }
             }
         }
-         if (idCorrect && !pwCorrect) {
-            answer = "wrong pw";
-        } else if (!idCorrect && !pwCorrect) {
-            answer = "fail";
-        }
         
-        
-        return answer;
+        return "fail";
     }
 }
