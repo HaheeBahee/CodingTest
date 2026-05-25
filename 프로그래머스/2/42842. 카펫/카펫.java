9f80e2707440d8dbe894040f15b7a927e205bb4e
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2026-05-25 12:44:34 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,14 +1,18 @@
+// brown : 갈색 격자 수, yellow : 노란색 격자 수
+// 아웃풋 : [가로크기][세로크기]
+
+// 공식 : 두 정수 곱해서 옐로우 되는 짝 a,b => (a+2)*(b+2) - yellow = brown 되는 거 찾기
+
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int total = brown + yellow;
-        for (int h = 3; h * h <= total; h++) {
-            if (total % h != 0) continue;
-            int w = total / h;
-            if (w < h) continue;              
-            if ((w - 2) * (h - 2) == yellow) {
-                return new int[]{w, h};
+        for (int i = 1; i <= yellow; i++) {
+            if (yellow % i == 0) {
+                int a = i, b = yellow / i;
+                if (a >= b && (a + 2) * (b + 2) - yellow == brown) {
+                    return new int[]{a + 2, b + 2};
+                }
             }
         }
-        return new int[]{0, 0};
+        return new int[]{};
     }
 }
