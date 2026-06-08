79045d1facde07cfe866ac300f51a8deadf2aa40
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2026-06-08 14:12:09 +0900

    [level 2] Title: 전화번호 목록, Time: 181.51 ms, Memory: 235 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,20 @@
+// A 번호가 B 번호의 접두어 => false
+
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String pre : phone_book){
+            map.put(pre, 1);
+        }
+        
+        for(String number : phone_book){
+            for(int i = 0; i < number.length(); i++){
+                String sub = number.substring(0, i);
+                if(map.containsKey(sub)) return false;
             }
         }
         return true;
