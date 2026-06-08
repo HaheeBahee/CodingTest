9ff77abbfe6d54e2cf7d878d985eaf3dec92e836
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2026-06-08 13:52:16 +0900

    [level 1] Title: 폰켓몬, Time: 4.28 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,21 @@
+// 최대한 많은 종류 가져가기. N/2마리 가져가기(N은 짝수)
+// 중복 있음 -> 해시, size로 종류
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-
-        Set<Integer> kinds = new HashSet<>();
-        for (int n : nums) {
-            kinds.add(n);
+        
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        int canPick = nums.length / 2;   
-        return Math.min(kinds.size(), canPick);
+        
+        int half = nums.length / 2;
+        if(map.size() >= half){
+            return half;
+        }else{
+            return map.size();
+        }
     }
 }
