6023f62fe35bfc75da5722f2b3b557890f26c2ca
HaheeBahee <164337172+HaheeBahee@users.noreply.github.com>
2026-06-08 16:02:42 +0900

    [level 2] Title: 의상, Time: 0.25 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,18 +1,23 @@
+// 매일 다른 옷 조합(일부 겹쳐도 돼) , 종류별 최대 1가지 
+
 import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> cntByType = new HashMap<>();
         
-        for (String[] item : clothes) {
-            String type = item[1];
-            cntByType.put(type, cntByType.getOrDefault(type, 0) + 1);
+        Map<String, Integer> map = new HashMap<>();
+        
+        // 카테고리 -> 키
+        for(String[] item : clothes){
+            map.put(item[1] , map.getOrDefault(item[1], 0) + 1);
         }
         
-        int ways = 1;
-        for (int cnt : cntByType.values()) {
-            ways *= (cnt + 1);
+        int result = 1;
+        for(int count : map.values()){
+            result *= (count + 1);
         }
-        return ways - 1; 
+        
+        return result - 1;
+        
     }
 }
